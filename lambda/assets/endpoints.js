d6c31de7b0553270df9b8cc88633d488a7231686
Federico <federicogerardi94@gmail.com>
2018-12-13 10:37:55 +0100

    Syntax bugfix

--- lambda/assets/endpoints.js
+++ lambda/assets/endpoints.js
@@ -1,5 +1,5 @@
 module.exports = {
-    BASE_URI = 'https://ctftime.org/api/v1',
+    BASE_URI: 'https://ctftime.org/api/v1',
     topEndPoint(year) {
         return `${BASE_URI}${year ? `${year}/` : '/'}`;
     }
