f44ceb4d3bb86954de9a32a33358fcdc5a54c9bc
Federico <federicogerardi94@gmail.com>
2018-12-18 23:27:14 +0100

    Handlers splitting ended and a new project structure is on

---
+++ lambda/eventsHandlers.js
@@ -0,0 +1,80 @@
+'use stricts'
+const helpers = require('./helpers');
+const messages = require('./assets/strings');
+const endpoints = require('./assets/endpoints');
+const request = require('request');
+const Speech = require('ssml-builder');
+
+module.exports.GetNextEvents = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        return request.type === 'LaunchRequest' || ( request.type === 'IntentRequest' && request.intent.name === 'GetNextEvents');
+    },
+    handle(handlerInput) {
+        const number = handlerInput.requestEnvelope.request.intent.slots.number.value || 5;
+        //insert a startByDate slot to look for starting from a specific date
+        const date = new Date();
+        return new Promise((resolve) => {
+            getNextEvents(date, number)
+            .then((events) => resolve(handlerInput.responseBuilder.speak(events).reprompt(events).getResponse()))
+            .catch((e) => {
+                console.log(`Error: ${e.message}`);
+                resolve(handlerInput.responseBuilder.speak(messages.INFORMATIONS.ERROR_MESSAGE).reprompt(messages.INFORMATIONS.ERROR_MESSAGE).getResponse());
+            });
+        })
+    }
+};
+function getNextEvents(startDate, number) {
+    return new Promise((resolve, reject) => {
+        requestNextEventsSet(startDate, number)
+        .then((events) => {
+            console.log(`Events received: ${JSON.stringify(events)}`);
+            let speech = new Speech();
+            speech.sentence(messages.INFORMATIONS.NEXT_EVENTS.START_MESSAGE.replace('{1}', events.length));
+            for(let i = 0; i < events.length; i++)
+            {
+                const and = (i === events.length - 1 && events.length !== 1);
+                const event = events[i];
+                //to refactor in a better way
+                speech.sayWithSSML(`<s>${getEventString(event, and)}</s>`);
+            }
+            resolve(speech.ssml(true));
+        })
+        .catch((e) => reject(e));
+    });
+}
+function getEventString(event, and) {
+    const title = event.title;
+    const type = event.format;
+    const eventStartDate = getDateString(event.start);
+    const duration = getDurationString(event.duration);
+    return `${and ? ' e ' : ''}${messages.INFORMATIONS.NEXT_EVENTS.EVENT_DESCRIPTION.replace('{1}', title).replace('{2}', type).replace('{3}', eventStartDate).replace('{4}', duration)}`;
+}
+function getDateString(dateString) {
+    const date = new Date(dateString);
+    const day = date.getDate();
+    //Javascript month enumeration fixing...
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    //this should be fixed using the ssml library
+    return `<say-as interpret-as="date" format="dmy">${day}/${month}/${year}</say-as>`;
+}
+function getDurationString(duration) {
+    return `${duration.days !== 0 ? duration.days : ''}${duration.hours !== 0 ? duration.hours : ''}`;
+}
+function requestNextEventsSet(startDate, number) {
+    return new Promise((resolve, reject) => {
+        //UNIX timestamp adjusting...
+        const startDateTime = startDate.getTime() / 1000;
+        let options = helpers.getCTFTimeRequestOptions(endpoints.eventsEndPoint());
+        //GET request query sting's parameters
+        options.qs = {};
+        if(startDateTime) options.qs.start = startDateTime;
+        if(number) options.qs.limit = number;
+        request(options, (err, res, body) => {
+            console.log(`Fetched data: ${JSON.stringify(body)}`);
+            if(err) reject(err);
+            else resolve(body);
+        });
+    });
+}
---
+++ lambda/genericsHandlers.js
@@ -0,0 +1,68 @@
+'use stricts'
+const messages = require('./assets/strings');
+
+module.exports.HelpHandler = {
+    canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest'
+      && request.intent.name === 'AMAZON.HelpIntent';
+  },
+  handle(handlerInput) {
+    return handlerInput.responseBuilder
+      .speak(messages.HELP_MESSAGE)
+      .reprompt(messages.HELP_REPROMPT)
+      .getResponse();
+  }
+};
+
+module.exports.FallbackHandler = {
+    canHandle(handlerInput) {
+      const request = handlerInput.requestEnvelope.request;
+      return request.type === 'IntentRequest'
+        && request.intent.name === 'AMAZON.FallbackIntent';
+    },
+    handle(handlerInput) {
+      return handlerInput.responseBuilder
+        .speak(messages.FALLBACK_MESSAGE)
+        .reprompt(messages.FALLBACK_REPROMPT)
+        .getResponse();
+    }
+  };
+  
+module.exports.ExitHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        return request.type === 'IntentRequest'
+            && (request.intent.name === 'AMAZON.CancelIntent'
+            || request.intent.name === 'AMAZON.StopIntent');
+    },
+    handle(handlerInput) {
+      return handlerInput.responseBuilder
+        .speak(messages.STOP_MESSAGE)
+        .getResponse();
+    }
+};
+  
+module.exports.SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        return request.type === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+        return handlerInput.responseBuilder.getResponse();
+    }
+};
+
+module.exports.ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        console.log(`Error handled: ${error.message}`);
+        return handlerInput.responseBuilder
+            .speak(messages.ERROR_REQUEST_MESSAGE)
+            .reprompt(messages.ERROR_REQUEST_MESSAGE)
+            .getResponse();
+    }
+};
