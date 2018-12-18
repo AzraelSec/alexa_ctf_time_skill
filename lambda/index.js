2d44bc2fb581af4b783e8ea22f6b3266d898be0d
Federico <federicogerardi94@gmail.com>
2018-12-18 22:43:15 +0100

    year slot in GetTopTeam(s) fixed; Testing the GetNextEvents handler...

--- lambda/index.js
+++ lambda/index.js
@@ -13,7 +13,7 @@ const GetTopTeamsHandler = {
     },
     handle(handlerInput) {
         let slots = handlerInput.requestEnvelope.request.intent.slots;
-        let year = slots.year.value || (new Date().getFullYear());
+        let year = (slots.year.value && slots.year.value !== '?') || (new Date().getFullYear());
         let number =  slots.number.value || 10;
         return new Promise((resolve) => {
             getTopTeams(year, number)
@@ -47,8 +47,9 @@ const GetTopTeamHandler = {
         return request.type === 'LaunchRequest'|| (request.type === 'IntentRequest' && request.intent.name === 'GetTopTeam');
     },
     handle(handlerInput) {
-        let year = handlerInput.requestEnvelope.request.intent.slots.year.value || (new Date().getFullYear());
-        return new Promise((resolve, reject) => {
+        let slots = handlerInput.requestEnvelope.request.intent.slots;
+        let year = (slots.year.value && slots.year.value !== '?') || (new Date().getFullYear());
+        return new Promise((resolve) => {
             getTopTeam(year)
             .then((value) => resolve(handlerInput.responseBuilder.speak(value).reprompt(value).getResponse()))
             .catch((e) => {
@@ -96,16 +97,20 @@ const GetNextEvents = {
         return request.type === 'LaunchRequest' || ( request.type === 'IntentRequest' && request.intent.name === 'GetNextEvents');
     },
     handle(handlerInput) {
-        const number = handlerInput.requestEnvelope.request.intent.slots.number.value ? handlerInput.requestEnvelope.request.intent.slots.number.value : null;
+        const number = handlerInput.requestEnvelope.request.intent.slots.number.value || 5;
+        //insert a startByDate slot to look for starting from a specific date
         const date = new Date();
-        return new Promise((resolve, reject) => {
+        return new Promise((resolve) => {
             getNextEvents(date, null, number)
             .then((events) => resolve(handlerInput.responseBuilder.speak(events).reprompt(events).getResponse()))
-            .catch((e) => reject(handlerInput.responseBuilder.speak(e.message).reprompt(e.message).getResponse()));
+            .catch((e) => {
+                console.log(`Error: ${e.message}`);
+                resolve(handlerInput.responseBuilder.speak(messages.INFORMATIONS.ERROR_MESSAGE).reprompt(messages.INFORMATIONS.ERROR_MESSAGE).getResponse());
+            });
         })
     }
 };
-function getNextEvents(startDate, endDate, number) {
+function getNextEvents(startDate, number) {
     return new Promise((resolve, reject) => {
         requestNextEventsSet(startDate, endDate, number)
         .then((events) => {
@@ -114,8 +119,10 @@ function getNextEvents(startDate, endDate, number) {
             speech.sentence(messages.INFORMATIONS.NEXT_EVENTS.START_MESSAGE.replace('{1}', events.length));
             for(let i = 0; i < events.length; i++)
             {
+                const and = (i === events.length - 1 && events.length !== 1);
                 const event = events[i];
-                speech.sayWithSSML(`<s>${getEventString(event, (i === events.length - 1 && events.length !== 1))}</s>`);
+                //to refactor in a better way
+                speech.sayWithSSML(`<s>${getEventString(event, and)}</s>`);
             }
             resolve(speech.ssml(true));
         })
@@ -127,7 +134,11 @@ function getEventString(event, and) {
     const type = event.format;
     const eventStartDate = getDateString(event.start);
     const duration = getDurationString(event.duration);
-    return `${and ? ' e ' : ''}${messages.INFORMATIONS.NEXT_EVENTS.EVENT_DESCRIPTION.replace('{1}', title).replace('{2}', type).replace('{3}', eventStartDate).replace('{4}', duration)}`;
+    return `${and ? ' e ' : ''}${messages.INFORMATIONS.NEXT_EVENTS.EVENT_DESCRIPTION
+        .replace('{1}', title)
+        .replace('{2}', type)
+        .replace('{3}', eventStartDate)
+        .replace('{4}', duration)}`;
 }
 function getDateString(dateString) {
     const date = new Date(dateString);
@@ -141,18 +152,17 @@ function getDateString(dateString) {
 function getDurationString(duration) {
     return `${duration.days !== 0 ? duration.days : ''}${duration.hours !== 0 ? duration.hours : ''}`;
 }
-function requestNextEventsSet(startDate, endDate, number) {
+function requestNextEventsSet(startDate, number) {
     return new Promise((resolve, reject) => {
         //UNIX timestamp adjusting...
-        let targetStartDate = startDate || (new Date().getTime() / 1000);
-        if(targetStartDate === '?') targetStartDate = (new Date().getTime() / 1000);
+        const startDateTime = startDate.getTime() / 1000;
         let options = getCTFTimeRequestOptions(endpoints.eventsEndPoint());
         //GET request query sting's parameters
         options.qs = {};
-        if(targetStartDate) options.qs.start = targetStartDate;
-        if(number) options.qs.limit = number || 5;
+        if(targetStartDate) options.qs.start = startDateTime;
+        if(number) options.qs.limit = number;
         request(options, (err, res, body) => {
-            console.log('Fetched data:' + JSON.stringify(body));
+            console.log(`Fetched data: ${JSON.stringify(body)}`);
             if(err) reject(err);
             else resolve(body);
         });
